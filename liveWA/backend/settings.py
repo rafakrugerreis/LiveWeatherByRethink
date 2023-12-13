ad23929d3264ed480aa35d9f3c7f8558f68d78fe
rafakrugerreis <rafael.reis@sou.unijui.edu.br>
2023-12-13 11:35:21 -0300

    Correções de problemas com CORS

--- liveWA/backend/settings.py
+++ liveWA/backend/settings.py
@@ -61,11 +61,7 @@ MIDDLEWARE = [
     
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",  
-]
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'liveWA.urls'
 
